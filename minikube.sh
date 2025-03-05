3115d630e68fa6d4b627e5b4eaff349719b5d815
japraz <158378288+japraz@users.noreply.github.com>
2025-03-05 15:11:58 +0100

    Update minikube.sh

--- minikube.sh
+++ minikube.sh
@@ -1,18 +1,42 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/bash
+
+ARCH=$(arch)
+
+### installing Docker
 sudo apt-get update -y
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
-sudo apt-get install -y docker-ce
-sudo usermod -aG docker "$USER"
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo touch /etc/apt/sources.list.d/kubernetes.list
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update -y
-sudo apt-get install -y kubectl
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
-sudo dpkg -i minikube_latest_amd64.deb
-minikube start --driver=docker
-minikube addons enable dashboard
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+if [ $ARCH = "x86_64" ]
+then
+	echo executing on $ARCH
+	#sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+	#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	#sudo apt-get update -y
+	#sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+	curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+	chmod +x ./kubectl
+	sudo mv ./kubectl /usr/local/bin/kubectl
+
+	curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+	sudo install minikube-linux-amd64 /usr/local/bin/minikube
+fi
+
+if [ $ARCH = "aarch64" ]
+then
+	curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-arm64
+	sudo install minikube-linux-arm64 /usr/local/bin/minikube
+	sudo snap install kubectl --classic	
+fi
+
+echo the script is now ready
+echo manually run minikube start to start it
+
+sudo usermod -aG docker $USER
+newgrp docker
